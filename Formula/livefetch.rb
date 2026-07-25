560bc16ce17e70fdbb44502c7564f13ceac4920e
WilGulf <wilgot.ulfstedt@outlook.com>
2026-07-25 21:04:17 +0200

    Updated livefetch to v1.0.1

--- Formula/livefetch.rb
+++ Formula/livefetch.rb
@@ -1,8 +1,8 @@
 class Livefetch < Formula
-    desc "A TUI program like fastfetch that actively updates the shown system information."
+    desc "A TUI system information tool with live-updating modules."
     homepage "https://github.com/WilGulf/livefetch"
-    url "https://github.com/WilGulf/livefetch/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "f155617165f57486e7f0a03a428296e32b9e93277d4cdd302c8d238a5bd89707"
+    url "https://github.com/WilGulf/livefetch/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "d565dc8e5c643e16d351a1a17e7b1b675e0ed031ed13d34cb6472bd145459476"
     license "MIT"
 
     depends_on "ncurses"

3f7e88d4339f3d0a2dd2b198287a45f4d7fc1793
WilGulf <wilgot.ulfstedt@outlook.com>
2026-08-06 18:05:46 +0200

    Fix Homebrew formula audit warning

--- Formula/livefetch.rb
+++ Formula/livefetch.rb
@@ -4,7 +4,6 @@ class Livefetch < Formula
   url "https://github.com/WilGulf/livefetch/archive/refs/tags/v2.0.0.tar.gz"
   sha256 "91607f9b904085c837f8bcf6a3901f22e8ceda0f44e7970761e9ba1d640a4b87"
   license "MIT"
-  
   head "https://github.com/WilGulf/livefetch.git", branch: "main"
 
   depends_on "ncurses"

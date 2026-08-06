6fd992af6ac7dcc0ee1c47111429c819153cdb7e
WilGulf <wilgot.ulfstedt@outlook.com>
2026-08-06 18:03:30 +0200

    Fix Homebrew formula audit warning

--- Formula/livefetch.rb
+++ Formula/livefetch.rb
@@ -1,8 +1,8 @@
 class Livefetch < Formula
   desc "TUI system information tool with live-updating modules"
   homepage "https://github.com/WilGulf/livefetch"
-  head "https://github.com/WilGulf/livefetch.git", branch: "main"
   url "https://github.com/WilGulf/livefetch/archive/refs/tags/v2.0.0.tar.gz"
+  head "https://github.com/WilGulf/livefetch.git", branch: "main"
   sha256 "91607f9b904085c837f8bcf6a3901f22e8ceda0f44e7970761e9ba1d640a4b87"
   license "MIT"
 

0a9b5db88c8cd3bf538633369e6e2bedb75b5e77
WilGulf <wilgot.ulfstedt@outlook.com>
2026-08-12 12:25:58 +0200

    Fixed sha256 sum

--- Formula/livefetch.rb
+++ Formula/livefetch.rb
@@ -2,7 +2,7 @@ class Livefetch < Formula
   desc "TUI system information tool with live-updating modules"
   homepage "https://github.com/WilGulf/livefetch"
   url "https://github.com/WilGulf/livefetch/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "e11ad6e32e62ccb93ce0c5af7163874cc6261e74f413b8ef3a00cd49ad6974e8"
+  sha256 "5c7f09937b5330ca78f45de2f4705d2c2769a744cea787a39240d9b6244728eb"
   license "MIT"
   head "https://github.com/WilGulf/livefetch.git", branch: "main"
 
